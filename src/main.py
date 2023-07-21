64c398747c5118452de38cf38a89fae184fbdac8
Moritz <moritz.deiaco@gmail.com>
2023-07-21 17:19:45 +0200

    some small changes

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_file
+from flask import Flask, request, jsonify, send_file, request
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
@@ -14,13 +14,11 @@ app = Flask(__name__)
 def gt_image():
     img = Image.new('RGB', (1000, 1000), (0, 0, 0))
 
-    font = ImageFont.truetype(filename='msyhbd.ttf', size=30)
-
     pen = ImageDraw.Draw(img)
 
     Geometry.rectangle(pen, 100, 100, 200, 200, (255, 155, 0))
     Geometry.ellipse(pen, 100, 100, 200, 200, (255, 0, 0))
-    Geometry.polygon(pen, 100, 100, 100, 6, 0, (0, 255, 0))
+    Geometry.polygon(pen, 100, 100, 100, 7, 0, (0, 255, 0))
 
     image_binary = BytesIO()
     img.save(image_binary, "PNG")
