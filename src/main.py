73ef75821d27aa8ad8f7ab7ea44415ca7d000e1c
Moritz <moritz.deiaco@gmail.com>
2023-07-24 12:49:16 +0200

    code is now able to read and evaluate a json file and render the geometry in it...

--- src/main.py
+++ src/main.py
@@ -9,22 +9,46 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def gt_image():
-    img = Image.new('RGB', (1000, 1000), (0, 0, 0))
+def get_image():
+
+    data = json.load(open('src/test.json'))
+
+    width = data["size"]["width"]
+    height = data["size"]["height"]
+
+    data_bgc = data["background-color"]
+
+    bgcolor = (data_bgc["R"], data_bgc["G"], data_bgc["B"])
+
+    img = Image.new('RGB', (width, height), bgcolor)
 
     pen = ImageDraw.Draw(img)
 
-    text = "ALWAYS BE HAPPY(LAUGHING IS THE BEST MEDICINE)"
+    geometry_data = data["geometry"]
 
-    pen.text
+    for geo_name, geo_data in data["geometry"].items():
+        arr = geo_name.split("_")
+        geo_name = arr[0]
+        if geo_name == "rect":
 
-    Geometry.rectangle(pen, 100, 100, 200, 200, (255, 155, 0))
-    Geometry.ellipse(draw=pen, x=100, y=100, height=200,
-                     width=200, rgb=(255, 0, 0))
-    Geometry.polygon(draw=pen, x=100, y=100, radius=100,
-                     n_sites=7, rotation=0, rgb=(0, 255, 0))
-    Geometry.text(draw=pen, x=100, y=100, size=50, text=text, words_per_line=30,
-                  align="left", rgb=(255, 255, 255), font="sans-serif")
+            Geometry.rectangle(draw=pen, x=geo_data["x"], y=geo_data["y"], width=geo_data["width"],
+                               height=geo_data["height"], rgb=(geo_data["color"]["r"], geo_data["color"]["g"], geo_data["color"]["b"]))
+        elif geo_name == "ellipse":
+
+            Geometry.ellipse(draw=pen, x=geo_data["x"], y=geo_data["y"], width=geo_data["width"],
+                             height=geo_data["height"], rgb=(geo_data["color"]["r"], geo_data["color"]["g"], geo_data["color"]["b"]))
+        elif geo_name == "polygon":
+
+            Geometry.polygon(draw=pen, x=geo_data["x"], y=geo_data["y"], radius=geo_data["radius"],
+                             n_sites=geo_data["sites"], rotation=geo_data["rotation"], rgb=(geo_data["color"]["r"], geo_data["color"]["g"], geo_data["color"]["b"]))
+
+        elif geo_name == "text":
+
+            Geometry.text(draw=pen, x=geo_data["x"], y=geo_data["y"], size=geo_data["size"], text=geo_data["text"], words_per_line=geo_data["words_per_line"],
+                          align=geo_data["align"], rgb=(geo_data["color"]["r"], geo_data["color"]["g"], geo_data["color"]["b"]), font=geo_data["font_style"])
+
+        else:
+            continue
 
     image_binary = BytesIO()
     img.save(image_binary, "PNG")
